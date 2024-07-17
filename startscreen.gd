efb530afc85008ebdbeb482ccaae936680134e05
James Herr <79765141+SeveralHerr@users.noreply.github.com>
2024-07-16 20:26:24 -0500

    Startscreen

---
+++ startscreen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+const CHARACTER_SELECT: PackedScene = preload("res://character_select.tscn")
+var delay: bool = false
+
+func _ready() -> void:
+	$Timer.timeout.connect(_delay)
+
+func _input(event: InputEvent) -> void:
+	if delay:
+		if event is InputEventMouseButton and event.pressed:
+			get_tree().change_scene_to_packed(CHARACTER_SELECT)
+		elif event is InputEventKey and event.pressed:
+			get_tree().change_scene_to_packed(CHARACTER_SELECT)
+		
+func _delay() -> void: 
+	delay = true
