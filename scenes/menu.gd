028c10fde1fa260f3a537ddbcd219c9c47c03e9b
James Herr <79765141+SeveralHerr@users.noreply.github.com>
2025-05-31 11:47:42 -0500

    Resolving editor errors

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -4,11 +4,11 @@ extends Control
 @onready var button: Button = $MarginContainer/VBoxContainer/Button
 @onready var exit_button: Button = $MarginContainer/VBoxContainer/ExitButton
 
-const CHARACTER_SELECT = preload("res://character_select.tscn")
+#const CHARACTER_SELECT = preload("res://character_select.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	button.pressed.connect(_return)
+	#button.pressed.connect(_return)
 	exit_button.pressed.connect(_close)
 	pass # Replace with function body.
 
@@ -18,8 +18,7 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _return() -> void: 
-	get_tree().change_scene_to_packed(CHARACTER_SELECT)
+
 	
 func _close() -> void:
 	queue_free()
