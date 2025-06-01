d4f66264d7b72322981de5a6b1a05fa17ed720e8
James Herr <79765141+SeveralHerr@users.noreply.github.com>
2025-06-01 00:48:38 -0500

    Added ryan

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -8,11 +8,11 @@ signal boss_fight(status: bool)
 signal event(status: bool)
 signal newspaper(status: bool)
 
-var selected_character: String = "Cody"
+var selected_character: String = "Ryan"
 var character_dict = {
 		"Robot": Unlockable.new("Robot", "ROBOT IS NOW PLAYABLE", true, "Atomic Robot Tattoo Mascot", preload("res://sprites/cody_sprite_frames.tres"), ""),
 		"Cody": Unlockable.new("Cody", "CODY IS NOW PLAYABLE", true, "Owner of Atomic Robot Tattoo", preload("res://Sprites/cody_sprite_frames.tres"), ""),
-		"Ryan": Unlockable.new("Ryan", "RYAN IS NOW PLAYABLE", true, "Employee of Atomic Robot Tatoo", preload("res://Sprites/cody_sprite_frames.tres"), ""),
+		"Ryan": Unlockable.new("Ryan", "RYAN IS NOW PLAYABLE", true, "Employee of Atomic Robot Tatoo", preload("res://Sprites/ryan_sprite_frames.tres"), ""),
 	}
 
 var player: Player
