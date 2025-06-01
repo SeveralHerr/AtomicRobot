becc025ef1dcbabee2738343575273610a5d28e5
James Herr <79765141+SeveralHerr@users.noreply.github.com>
2025-05-31 19:03:38 -0500

    Actually fixed car collision this time probably

--- scripts/car.gd
+++ scripts/car.gd
@@ -19,10 +19,10 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if start:
-		_enable_player_layer(true)
+
 		show()
 		if speed == 0:
-			
+			_enable_player_layer(true)
 			randomize()
 			speed = randi_range(150,450)
 			print("new speed ", speed)
@@ -35,12 +35,16 @@ func _process(delta: float) -> void:
 
 func _hit(body: Node2D) -> void:
 	if body is Player:
+		
 		shaker.apply_shake(2)
 		_enable_player_layer(false)
 		print("Car hit")
 		body.receive_hit(global_position, car_damage)
 
 		speed = 0
+		#await get_tree().create_timer(3).timeout
+		#start=false
 		
 func _enable_player_layer(can_hit: bool) -> void:
 	area_2d.set_collision_mask_value(1, can_hit)
+	area_2d.set_collision_mask_value(5, can_hit)
