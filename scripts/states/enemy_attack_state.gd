3bbe232c0856ecb96715faf901afe96f7d919e50
James Herr <79765141+SeveralHerr@users.noreply.github.com>
2025-06-05 02:35:31 -0500

    misc

--- scripts/states/enemy_attack_state.gd
+++
@@ -1,8 +0,0 @@
-extends AttackPlayerState
-class_name EnemyAttackState
-
-## Extended attack state with longer range for special enemies like TrainMeterMaid
-
-func enter_state(enemy: Enemy) -> void:
-	enemy.attack_range = 250  # Extended range for special enemies
-	super.enter_state(enemy)
