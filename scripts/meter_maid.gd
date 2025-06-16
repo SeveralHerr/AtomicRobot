120d86d4c8a01b154e2f62961bb2a7094ecd9cab
James Herr <79765141+SeveralHerr@users.noreply.github.com>
2025-06-16 18:36:46 -0500

    New assets

--- scripts/meter_maid.gd
+++ scripts/meter_maid.gd
@@ -1,8 +1,11 @@
 extends Enemy
 class_name MeterMaid
 
+
+
 func _ready() -> void:
 	super._ready()
+	coins = 1
 	enemy_state_machine.add_state("PatrolState", PatrolState.new(self))
 	enemy_state_machine.add_state("ChasePlayerState", ChasePlayerState.new(self))
 	enemy_state_machine.add_state("AttackPlayerState", AttackPlayerState.new(self))
